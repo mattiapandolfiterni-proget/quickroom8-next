9cdf317b90919cb272eeb11b99e1f0d998a55253
Mattia Pandolfi <mattiapandolfiterni@gmail.com>
2025-12-23 00:02:34 +0100

    FIX: Add vercel.json, .nvmrc, engine spec for Vercel

--- next.config.js
+++ next.config.js
@@ -1,27 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Ignora errori di TypeScript durante il build
-    typescript: {
-      ignoreBuildErrors: true,
-    },
-    // Ignora errori di stile (ESLint) durante il build
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-    // Evita che le immagini facciano crashare il sito
-    images: {
-      unoptimized: true,
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "**",
-        },
-      ],
-    },
-    // Evita errori strani se usi Suspense
-    experimental: {
-      missingSuspenseWithCSRBailout: false,
-    },
-  };
-  
-  module.exports = nextConfig; 
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
